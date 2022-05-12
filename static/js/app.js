73e40963e11ed1643e7cbb2ded45fc84af53a244
yaritza <colonyaritza@yahoo.com>
2022-05-12 12:32:53 -0500

    update file

--- static/js/app.js
+++ static/js/app.js
@@ -67,7 +67,7 @@ function updateFilters() {
   }
   
   // 2. Attach an event to listen for changes to each filter
-  d3.selectAll("input").on("changes",updateFilters);
+  d3.selectAll("input").on("change",updateFilters);
 
   
   // Build the table when the page loads
